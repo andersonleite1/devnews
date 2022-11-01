17990210c72d4d9303c5d66d8840752b0343340b
Anderson Leite <andersonleite.dev@gmail.com>
2022-11-01 15:45:39 -0300

    feat: adds dynamic title to the Home

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,12 @@
+import Head from "next/head"
+
 export default function Home() {
   return (
-    <h1>Start development project</h1>
+    <>
+      <Head>
+        <title>Home | ig.news</title>
+      </Head>
+      <h1>Home</h1>
+    </>
   )
 }
