edd4d526179e0f2f403a4962438c9f58f0be056a
Anderson Leite <andersonleite.dev@gmail.com>
2022-11-02 08:11:52 -0300

    feat: add component Header at app

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,13 @@
 import type { AppProps } from 'next/app'
+import { Header } from '../components/Header'
+
 import '../styles/global.scss'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Header />
+      <Component {...pageProps} />
+    </>
+  )
 }
