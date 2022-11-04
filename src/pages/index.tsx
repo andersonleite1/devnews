1f1a9af71cb89c73310a438ab1cca5ee82ca0422
Anderson Leite <andersonleite.dev@gmail.com>
2022-11-04 11:10:12 -0300

    feat: create page Home

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,5 @@
 import Head from "next/head"
+import styles from "./home.module.scss"
 
 export default function Home() {
   return (
@@ -6,7 +7,18 @@ export default function Home() {
       <Head>
         <title>Home | ig.news</title>
       </Head>
-      <h1>Home</h1>
+      
+      <main className={styles.container}>
+        <section className={styles.hero}>
+          <span>👏 Hey, welcome</span>
+          <h1>News about the <span>React</span> world.</h1>
+          <p>
+            Get access to all the publications <br />
+            <span>for $2.90</span> month
+          </p>
+        </section>
+        <img src="/images/women.svg" alt="Girl coding" />
+      </main>
     </>
   )
 }
