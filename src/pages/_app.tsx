7a9dd7ae92e6698522f30cb2025c15b099eaa669
Anderson Leite <andersonleite.dev@gmail.com>
2022-11-11 09:29:23 -0300

    feat: add SessionProvider at page _app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,13 +1,14 @@
 import type { AppProps } from 'next/app'
 import { Header } from '../components/Header'
+import { SessionProvider } from 'next-auth/react'
 
 import '../styles/global.scss'
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <>
+    <SessionProvider session={pageProps.session}>
       <Header />
       <Component {...pageProps} />
-    </>
+    </SessionProvider>
   )
 }
