c3eb7b20c113d383d094fd2224b16052d6e8bec2
Anderson Leite <andersonleite.dev@gmail.com>
2022-11-12 17:02:26 -0300

    feat:  saves user in the database

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,12 +1,28 @@
 import NextAuth from "next-auth";
 import GithubProvider from "next-auth/providers/github";
 
+import { query as qr } from "faunadb";
+
+import { faunaClient } from "../../../services/fauna";
+
 const clientId = process.env.GITHUB_ID as string;
 const clientSecret = process.env.GITHUB_SECRET as string;
 
+ interface signInProps {
+  user: {
+    email: string;
+  };
+  account: {
+    provider: string;
+  };
+  profile: {
+    email: string;
+  };
+}
+
 if (!clientId || !clientSecret) throw new Error("Missing GitHub credentials");
 
-export const authOptions = {
+export default NextAuth({
   providers: [
     GithubProvider({
       clientId,
@@ -18,6 +34,21 @@ export const authOptions = {
       }
     }),
   ],
-};
-
-export default NextAuth(authOptions);
+  callbacks: {
+    async signIn({ user, account, profile }: signInProps) {
+      const { email } = user;
+      try {
+        await faunaClient.query(
+          qr.Create(qr.Collection("users"), {
+            data: { email },
+          })
+        );
+        return true;
+      }
+      catch (error) {
+        console.log(error);
+        return false;
+      }
+    }
+  }
+});
