18486fd5886159637369037c8a933e99100b45d3
Anderson Leite <andersonleite.dev@gmail.com>
2022-11-12 16:59:25 -0300

    fix: export const faunaClient of fauna.ts

--- src/services/fauna.ts
+++ src/services/fauna.ts
@@ -2,6 +2,6 @@ import { Client } from "faunadb"; 'faunadb';
 
 const FAUNA_SECRET_KEY = process.env.FAUNA_SECRET_KEY || "";
 
-const faunaClient = new Client({
+export const faunaClient = new Client({
   secret: FAUNA_SECRET_KEY,
 });
