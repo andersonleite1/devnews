5e4ba6012a8127af324330c69048f59281013928
Anderson Leite <andersonleite.dev@gmail.com>
2022-12-12 20:59:55 -0300

    feat: redirects to Stripe

--- src/components/SubscribeButton/index.tsx
+++ src/components/SubscribeButton/index.tsx
@@ -1,4 +1,6 @@
 import { signIn, useSession } from 'next-auth/react';
+import { api } from '../../services/api';
+import { getStripeJs } from '../../services/stripe-js';
 import styles from './styles.module.scss';
 
 interface SubscribeButtonProps {
@@ -8,12 +10,29 @@ interface SubscribeButtonProps {
 export function SubscribeButton({ priceId }: SubscribeButtonProps) {
   const {status} = useSession();
 
-  function handleSubscribe() {
+  async function handleSubscribe() {
     if (status !== 'authenticated') {
       signIn('github');
       return;
     }
-    console.log('Subscribe');
+    
+    try {
+      const response = await api.post('/subscribe');
+      const { sessionId } = response.data;
+
+      const stripe = await getStripeJs();
+
+      if (!stripe) {
+        console.log('Stripe not found');
+        return;
+      }
+
+      await stripe.redirectToCheckout({ sessionId });
+    } catch (err) {
+      if (err instanceof Error) {
+        console.log(err.message);
+      }
+    }
     
   }
 
