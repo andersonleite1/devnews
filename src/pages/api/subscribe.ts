e75146ba08d6eab8029712c2c911e2692724d9cb
Anderson Leite <andersonleite.dev@gmail.com>
2022-12-12 21:45:59 -0300

    feat: avoids duplication of users on Stripe

--- src/pages/api/subscribe.ts
+++ src/pages/api/subscribe.ts
@@ -1,17 +1,57 @@
+import { query as qr } from "faunadb";
 import { NextApiRequest, NextApiResponse } from "next";
 import { getSession } from "next-auth/react";
+import { type } from "os";
+import { faunaClient } from "../../services/fauna";
 import { stripe } from "../../services/stripe";
 
+type User = {
+  ref: {
+    id: string;
+  },
+  data: {
+    stripe_customer_id: string;
+  }
+}
+
 export default async (req: NextApiRequest, res: NextApiResponse) => {
-  const session = await getSession({ req });
-
-  const stripeCustomer = await stripe.customers.create({
-    email: session?.user?.email || '',
-  });
-
   if (req.method === 'POST') {
+    const session = await getSession({ req });
+
+    if (!session?.user?.email) return res.status(401).end('Unauthorized');
+
+    const user = await faunaClient.query<User>(
+      qr.Get(
+        qr.Match(
+          qr.Index('user_by_email'),
+          qr.Casefold(session.user.email)
+        )
+      )
+    );
+
+    let customerId = user.data.stripe_customer_id;
+
+    if (!customerId) {
+      const stripeCustomer = await stripe.customers.create({
+        email: session.user.email,
+      });
+      
+      await faunaClient.query(
+        qr.Update(
+          qr.Ref(qr.Collection('users'), user.ref.id),
+          {
+            data: {
+              stripe_customer_id: stripeCustomer.id,
+            }
+          }
+        )
+      );
+
+      customerId = stripeCustomer.id;
+    }
+
     const stripeCheckoutSession = await stripe.checkout.sessions.create({
-      customer: stripeCustomer.id,
+      customer: customerId,
       payment_method_types: ['card'],
       billing_address_collection: 'required',
       line_items: [
