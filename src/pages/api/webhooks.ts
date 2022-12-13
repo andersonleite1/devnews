a85ebd4f65d24f5ead7f3d123ebef9adb5a623c7
Anderson Leite <andersonleite.dev@gmail.com>
2022-12-13 10:05:56 -0300

    feat: hears more Stripe webhooks events

--- src/pages/api/webhooks.ts
+++ src/pages/api/webhooks.ts
@@ -4,7 +4,6 @@ import Stripe from "stripe";
 import { stripe } from "../../services/stripe";
 import { saveSubscription } from "./_lib/manageSubscription";
 
-
 async function buffer(readable: Readable) {
   const chunks = [];
 
@@ -52,6 +51,17 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
     if (relevantEvents.has(type)) {
       try {
         switch (type) {
+          case 'customer.subscription.created':
+          case 'customer.subscription.updated':
+          case 'customer.subscription.deleted':
+            const subscription = event.data.object as Stripe.Subscription;
+
+            await saveSubscription(
+              subscription.id,
+              subscription.customer.toString(),
+              type === 'customer.subscription.created',
+            );
+            break;
           case 'checkout.session.completed':
             const checkoutSession = event.data.object as Stripe.Checkout.Session;
 
