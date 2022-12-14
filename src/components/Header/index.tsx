6667ca49193ef091d90caba24ad61886ea511fe0
Anderson Leite <andersonleite.dev@gmail.com>
2022-12-14 19:50:50 -0300

    feat: add ActiveLink at Header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,17 +1,19 @@
 import Link from 'next/link';
+import { useRouter } from 'next/router';
 import { SignInButton } from '../SignInButton';
 import styles from './styles.module.scss';
 
 export function Header () {
+  const { asPath } = useRouter();
   return (
     <header className={styles.headerContainer}>
       <div className={styles.headerContent}>
         <img src="/images/logo.svg" alt="ig.news" />
         <nav>
-          <Link href="/" className={styles.active}>
+          <Link href="/" className={asPath === '/' ? styles.active : ''}>
             Home
           </Link>
-          <Link href="posts" prefetch>
+          <Link href="posts" className={asPath === '/posts' ? styles.active : ''} prefetch>
             Posts
           </Link>
         </nav>
