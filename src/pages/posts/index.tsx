e90d02b5fa7e69e4521878cadb5cae29c68e6685
Anderson Leite <andersonleite.dev@gmail.com>
2022-12-15 08:45:01 -0300

    feat: add link for view post page full

--- src/pages/posts/index.tsx
+++ src/pages/posts/index.tsx
@@ -1,5 +1,6 @@
 import { GetStaticProps } from 'next';
 import Head from 'next/head';
+import Link from 'next/link';
 import { getPrismicClient } from '../../services/prismic';
 import Prismic from '@prismicio/client';
 import { RichText } from 'prismic-dom';
@@ -24,11 +25,11 @@ export default function Posts({ posts }: PostsProps) {
       <main className={styles.container}>
         <div className={styles.posts}>
          {posts.map(post => (
-           <a key={post.slug} href="#">
+           <Link key={post.slug} href={`posts/${post.slug}`}>
               <time>{post.updatedAt}</time>
               <strong>{post.title}</strong>
               <p>{post.content}</p>
-            </a>
+            </Link>
           ))}
         </div>
       </main>
