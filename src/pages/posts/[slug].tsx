854c0b928b7d97d26388c27d75909c7faeb01116
Anderson Leite <andersonleite.dev@gmail.com>
2022-12-15 08:43:18 -0300

    feat: create page view post full

---
+++ src/pages/posts/[slug].tsx
@@ -0,0 +1,73 @@
+import { GetServerSideProps } from "next";
+import { getSession } from "next-auth/react";
+import Head from "next/head";
+import { RichText } from "prismic-dom";
+import { getPrismicClient } from "../../services/prismic";
+
+import styles from './post.module.scss';
+
+interface PostProps {
+  post: {
+    slug: string;
+    title: string;
+    content: string;
+    updatedAt: string;
+  }
+}
+
+export default function Post({ post }: PostProps) {
+  return (
+    <>
+      <Head>
+        <title>{post.title} | dev.news</title>
+      </Head>
+      <main className={styles.conteiner}>
+        <article className={styles.post}>
+          <h1>{post.title}</h1>
+          <time>{post.updatedAt}</time>
+          <div
+            className={styles.postContent}
+            dangerouslySetInnerHTML={{ __html: post.content }}
+          />
+        </article>
+      </main>
+    </>
+  )
+}
+
+export const getServerSideProps: GetServerSideProps = async ({ req, params }) => {
+  const session = await getSession({ req });
+  
+  if (!params) {
+    console.log('No params');
+    return;
+  }
+
+  const { slug } = params;
+
+  const prismic = getPrismicClient(req);
+
+  const response = await prismic.getByUID('post', String(slug), {});
+
+  if (!response.last_publication_date) {
+    console.log('Post without publication date');
+    return;
+  }
+
+  const post = {
+    slug,
+    title: RichText.asText(response.data.title),
+    content: RichText.asHtml(response.data.content),
+    updatedAt: new Date(response.last_publication_date).toLocaleDateString('pt-BR', {
+      day: '2-digit',
+      month: 'long',
+      year: 'numeric',
+    }),
+  }
+
+  return {
+    props: {
+      post,
+    }
+  }
+}
